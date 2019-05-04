370bed290178ebe4d4743db49e8d777ffaa0b96b
Kappeh <kieranleep@hotmail.com>
2019-05-04 17:57:10 +0100

    Created player base class

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    
+    def __init__(self, game, name = 'Player'):
+        self.game = game
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def get_action(self, state):
+        pass
